610826188ceda7f8b64cac3a20eb4812635645aa
Veronica <almeida.vrc@gmail.com>
2023-07-22 11:48:59 -0300

    Criação da classe Contaterminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner sc = new Scanner(System.in);
+        Scanner scString = new Scanner(System.in);
+
+        int conta;
+        String agencia;
+        String nomeDoCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da conta");
+        conta = sc.nextInt();
+
+        System.out.println("Por favor, digite o número da agência");
+        agencia = scString.next();
+
+        System.out.println("Por favor, digite o nome do cliente");
+        nomeDoCliente = scString.next();
+
+        System.out.println("Por favor, digite o saldo");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá "+ nomeDoCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + conta + " e seu saldo " + String.format("%.2f", saldo)  + " já está disponível para saque");
     }
 }
